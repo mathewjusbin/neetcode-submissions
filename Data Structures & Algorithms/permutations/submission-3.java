726751a052bf6fdab647de7eaf37e377028b157d
mathewjusbin <mathewjusbin@gmail.com>
2026-09-16 06:15:58 +0530

    Add: permutations - submission-3

---
+++ Data Structures & Algorithms/permutations/submission-3.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> current = new ArrayList<>();
+        boolean[] used = new boolean[nums.length];
+        backtrack(used, current, nums, result);
+        return result;
+    }
+        public void backtrack(boolean[] used,List<Integer> current,int[] nums,List<List<Integer>> result){
+        if(current.size() == nums.length){
+            result.add(new ArrayList<>(current));//Why new of arraylist is we are calling each
+            //method with current so the value changes
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if(used[i]){
+                continue;
+            }
+            current.add(nums[i]);
+            used[i] = true;
+            backtrack(used,current,nums,result);
+
+            current.remove(current.size()-1);
+            used[i] = false;
+        }
+    }
+}
